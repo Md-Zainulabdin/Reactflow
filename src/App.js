4ae4aa436944f6a20a44465d3d813c2546ef757b
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-09-28 19:59:00 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactFlow from 'reactflow'
+import 'reactflow/dist/style.css';
 
-function App() {
+const MyFlow = () => {
+  const initialNode = [
+    {
+      id:'1',
+      data: {label: "Parent Node"},
+      type: "input",
+      position :{x:0, y:0}
+    },
+    {
+      id:'2',
+      data: {label: "Child Node"},
+      type: "output",
+      position :{x:0, y:0}
+    },
+    {
+      id:'3',
+      data: {label: "default Node"},
+      type: "default",
+      position :{x:50, y:50}
+    }
+  ];
+  const initialEdge = [{
+    id:'e1-2',
+    source: '1',
+    target: '2',
+    type: 'step',
+    label: '1 to 2'
+  }];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen'>
+      <ReactFlow
+      defaultEdges={initialEdge}
+      defaultNodes={initialNode}
+      ></ReactFlow>
     </div>
-  );
+  )
 }
-
-export default App;
+export default MyFlow;
